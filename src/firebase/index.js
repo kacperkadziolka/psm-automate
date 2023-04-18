57609565deb5e4155be45ccd0af1114477b9b23b
krkadziolka <s220692@student.uek.krakow.pl>
2023-04-18 16:55:46 +0200

    initalize the project environment

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPrW5R9gNM4FBcTJZRKolH6iZ9P-bDN2M',
+  authDomain: 'psm-automate-6133c.firebaseapp.com',
+  projectId: 'psm-automate-6133c',
+  storageBucket: 'psm-automate-6133c.appspot.com',
+  messagingSenderId: '306326679208',
+  appId: '1:306326679208:web:e10778e635d9c328c1dc9d'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
