4ab41e9bc8fa097d0dafc5e3b7dde058e0221a89
krkadziolka <s220692@student.uek.krakow.pl>
2023-04-21 18:23:19 +0200

    login page

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDPrW5R9gNM4FBcTJZRKolH6iZ9P-bDN2M',
   authDomain: 'psm-automate-6133c.firebaseapp.com',
@@ -13,5 +10,8 @@ const firebaseConfig = {
   appId: '1:306326679208:web:e10778e635d9c328c1dc9d'
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { auth }
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from "../views/LoginView.vue";
+import { getAuth } from "firebase/auth";
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: LoginView,
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+
+  if (to.matched.some((record) => record.meta.authRequired)) {
+    if (auth.currentUser) {
+      next();
+    } else {
+      alert("You've must been logged to access this area");
+      router.push("/");
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
