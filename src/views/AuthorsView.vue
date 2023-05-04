e9755d94072cea06127a7294b7622f0b48b77f53
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-05-04 22:36:01 +0200

    added authors as firebase collection

--- src/views/AuthorsView.vue
+++ src/views/AuthorsView.vue
@@ -1,57 +1,61 @@
 <template>
-    <div class="div-container">
-        <TopBar />
-      <div class="authors-container">
-        <div class="author-info">
-          <h2>Kacper Kądziołka</h2>
-          <p>Student ID: {{ kacperKadziolkaStudentID }}</p>
-        </div>
-        <div class="author-info">
-          <h2>Krzysztof Anyż</h2>
-          <p>Student ID: {{ krzysztofAnyzStudentID }}</p>
-        </div>
+  <div class="div-container">
+    <TopBar />
+    <div class="authors-container">
+      <div v-for="(author, index) in authors" :key="index" class="author-info">
+        <h2>{{ author.Name }} {{ author.Surname }}</h2>
+        <p>Student ID: {{ author.student_id }}</p>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import TopBar from '@/components/TopBar.vue'
-  
-  export default{
-    name: 'AuthorsView',
-    components: {
-      TopBar
-    },
-    data() {
-      return {
-        krzysztofAnyzStudentID: 's220034',
-        kacperKadziolkaStudentID: 's220692'
-      }
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
+
+export default {
+  name: 'AuthorsView',
+  components: {
+    TopBar
+  },
+  data() {
+    return {
+      authors: []
     }
+  },
+  async created() {
+    const authorsCollection = collection(db, 'authors')
+    const authorsSnapshot = await getDocs(authorsCollection)
+    authorsSnapshot.forEach((doc) => {
+      this.authors.push(doc.data())
+    })
   }
-  </script>
-  
-  <style scoped>
-  .authors-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin-top: 50px;
-  }
-  
-  .author-info {
-    text-align: center;
-    margin-bottom: 20px;
-  }
-  
-  .author-info h2 {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
-  
-  .author-info p {
-    font-size: 18px;
-    color: #666;
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.authors-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 50px;
+}
+
+.author-info {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.author-info h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.author-info p {
+  font-size: 18px;
+  color: #666;
+}
+</style>
