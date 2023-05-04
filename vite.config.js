7f82d6660245fddc6c31e82c0d58e872355671ee
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-05-04 23:05:15 +0200

    pwa test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'My PWA',
+        short_name: 'My PWA',
+        description: 'My Progressive Web App',
+        icons: [
+          {
+            src: '/assets/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+      },
+      workbox: {
+        // Workbox options
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,9 +41,5 @@ export default defineConfig({
   },
   server: {
     host: true,
-
   },
-
 })
-
-
