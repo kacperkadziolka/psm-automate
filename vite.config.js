612d92703601be6130c322d83ea1898a76d0bb73
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-05-07 13:41:54 +0200

    dist again and again

--- vite.config.js
+++ vite.config.js
@@ -9,56 +9,6 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Automate',
-        short_name: 'Automate',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images',
-              expiration: {
-                maxEntries: 20,
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:js|css)$/,
-            handler: 'StaleWhileRevalidate',
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 24 * 60 * 60, // 1 day
-              },
-            },
-          },
-        ],
-        skipWaiting: true,
-        clientsClaim: true,
-      },
     }),
   ],
   resolve: {
