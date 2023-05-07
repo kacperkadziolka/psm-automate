620bb929b2b80022bbb834345d06ae673c0acf83
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-05-07 13:04:06 +0200

    dist 2

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,15 +1,44 @@
-// Install service worker
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js'
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service worker installed');
-  });
-  
-  // Activate service worker
-  self.addEventListener('activate', (event) => {
-    console.log('Service worker activated');
-  });
-  
-  // Fetch from network or cache
-  self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-    event.respondWith(fetch(event.request));
-  });
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-pwa-cache-v1'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
